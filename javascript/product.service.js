9c361a3a04802def3091fd9e98198ae3e34b5a3c
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-30 13:22:53 -0500

    Created a service to be injected into the local storage controller.

---
+++ javascript/product.service.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('shop')
+    .factory('ProductService', ProductService);
+    console.log('hello');
+
+ function ProductService() {
+   return {};
+  }
+
+
+}());
