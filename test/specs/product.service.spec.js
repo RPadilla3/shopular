b483e539d29bec5cadec3cfd0a33ced2e11896c2
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-02 16:28:24 -0500

    created a passing test for the getAll function to add items into the array, still working on the localStorage test.

--- test/specs/product.service.spec.js
+++ test/specs/product.service.spec.js
@@ -6,6 +6,9 @@
 
   describe('ProductService', function () {
     var ProductService;
+    var items = [];
+
+    console.log('service test');
 
     beforeEach(module('shop'));
 
@@ -17,8 +20,22 @@
     it('Should be able to get an Array of Items', function() {
       var result = ProductService.getAll();
       expect(result).to.be.an('array');
+      expect(result.length).to.equal(12);
+      expect(result[0].name).to.equal('widget');
+      console.log(ProductService.update());
 
     });
+
+    describe('Saves array of items to localStorage', function(){
+      var items = [{}];
+
+
+      it('Should save an array to the localStorage', function(){
+        expect(JSON.parse(localStorage.getItems(items)).length).to.equal(1);
+          ProductService.update();
+      })
+    });
+
 
   });
 
