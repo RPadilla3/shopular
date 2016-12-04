9e8f64395ca3d6860ac96c04361a0345b80db9db
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-04 13:03:45 -0500

    Fixed code to proper indentation.

--- src/javascript/shop.controller.js
+++ src/javascript/shop.controller.js
@@ -11,24 +11,14 @@
   function ShopController(ProductService){
     this.sortOrder = 'price';
     this.newItem = {};
-
-      this.tax = 1.0575;
-
-      this.items = ProductService.getAll();
-
-      this.addProduct = function addProduct(product){
+    this.tax = 1.0575;
+    this.items = ProductService.getAll();
+    this.addProduct = function addProduct(product){
           ProductService.addNew(product);
-
-        };
-
-      this.sortCategory = function sortCategory(sortOrder){
+    };
+    this.sortCategory = function sortCategory(sortOrder){
         this.sortOrder = sortOrder;
-      };
-
-    }
-
-
-
-
+    };
+  }
 
 }());
